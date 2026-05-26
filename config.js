76319bf1a73f9d7d07b6f3e8f48f36bc946f546c
JIFU設計 <jifu.des01@gmail.com>
2026-05-26 13:49:02 +0800

    add real supabase config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // 吉富 DM 雲端正式版設定檔
 
 window.JIFU_SUPABASE_CONFIG = {
-  SUPABASE_URL: "https://fbsociyvtobtoeqnwauj.supabase.co",
+  SUPABASE_URL: "https://puahvvnohimkbcmkrsxc.supabase.co/rest/v1/",
 
-  SUPABASE_ANON_KEY: "sb_publishable_GnkBDLdz6XIMhiCAZjTcJA_T6QQdpLw"
+  SUPABASE_ANON_KEY: "sb_publishable_YA0D4gdnmuImRI0uQLWEyA_-hoJSNi3"
 };
