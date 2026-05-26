697ca148d856473d4d970cf18519d8b32f622706
JIFU設計 <jifu.des01@gmail.com>
2026-05-26 13:59:54 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
-// 吉富 DM 雲端正式版設定檔
-
-window.JIFU_SUPABASE_CONFIG = {
-  SUPABASE_URL: "https://puahvvnohimkbcmkrsxc.supabase.co/rest/v1/",
-
-  SUPABASE_ANON_KEY: "sb_publishable_YA0D4gdnmuImRI0uQLWEyA_-hoJSNi3"
+window.JIFU_SUPABASE = window.JIFU_SUPABASE || {
+  url: "https://puahvvnohimbbcmkrsxc.supabase.co",
+  anonKey: "sb_publishable_YA0D4gdnmuImRI0uQLWEyA_-hoJSNi3",
+  storageBucket: "jifu-dm-assets",
 };
