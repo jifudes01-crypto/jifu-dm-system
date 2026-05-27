fed3c7fbd5c464540cd9834da67bc5db6ce408f2
JIFU設計 <jifu.des01@gmail.com>
2026-05-27 10:42:03 +0800

    fix supabase config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-window.JIFU_SUPABASE_CONFIG = {
-  SUPABASE_URL: "https://fbsociyvtobtoeqnwauj.supabase.co",
-
-  SUPABASE_ANON_KEY: "sb_publishable_GnkBDLdz6XIMhiCAZjTcJA_T6QQdpLw"
+window.JIFU_SUPABASE = window.JIFU_SUPABASE || {
+  url: "https://fbsociyvtobtoeqnwauj.supabase.co",
+  anonKey: "sb_publishable_GnkBDLdz6XIMhiCAZjTcJA_T6QQdpLw",
+  storageBucket: "dm-assets"
 };
