6425f1d103eefe088233ada33c294540ca5766d7
JIFU設計 <jifu.des01@gmail.com>
2026-05-27 10:34:35 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.JIFU_SUPABASE = window.JIFU_SUPABASE || {
-  url: "https://puahvvnohimbbcmkrsxc.supabase.co",
-  anonKey: "sb_publishable_YA0D4gdnmuImRI0uQLWEyA_-hoJSNi3",
-  storageBucket: "jifu-dm-assets",
+  url: "https://fbsociyvtobtoeqnwauj.supabase.co",
+  anonKey: "sb_publishable_GnkBDLdz6XIMhiCAZjTcJA_T6QQdpLw",
+  storageBucket: "jifu-dm-system",
 };
